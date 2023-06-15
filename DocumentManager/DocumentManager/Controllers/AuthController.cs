2efbe09963bb3502521b7c120b831c411e08d3d1
licu <alex.chiuzan17@gmail.com>
2023-06-15 12:59:15 +0300

    Add Role Base functionally

--- DocumentManager/DocumentManager/Controllers/AuthController.cs
+++ DocumentManager/DocumentManager/Controllers/AuthController.cs
@@ -50,7 +50,8 @@ namespace DocumentManager.Controllers
         {
             List<Claim> claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.Username)
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Admin")
             };
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
 
